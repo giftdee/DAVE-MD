0883a307d697748443b88278b6fc408dc89ff1c2
giftdee <discordbotz526@gmail.com>
2025-08-30 03:55:40 +0300

    Delete commands directory
    Signed-off-by: giftdee <discordbotz526@gmail.com>

--- commands/unmute.js
+++
@@ -1,6 +0,0 @@
-async function unmuteCommand(dave, chatId) {
-    await dave.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
-    await dave.sendMessage(chatId, { text: 'The group has been unmuted.' });
-}
-
-module.exports = unmuteCommand;
